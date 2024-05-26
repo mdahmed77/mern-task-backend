80977920f934716f9326933ba6f0230a05cbc9bf
Daniyal Ahmed <45146013+mdahmed77@users.noreply.github.com>
2024-05-26 23:44:02 +0500

    Update

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors({
-    origin: ["http://localhost:5000", "http://merntaskapp.onrender.com"]
+    origin: ["http://localhost:5000", "http://merntaskmaster.netlify.com"]
 }));
 app.use("/api/tasks", taskRoutes);
 
